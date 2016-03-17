b4094e3ddf93be4048740896a53121fdc9053134
a944409629 <944409629@qq.com>
2016-03-17 11:17:18 +0800

    IOS计算x+y＝z
    计算x+y=z

--- ADD/ViewController.swift
+++ ADD/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func ADD(sender: UIButton) {
+        var a:Double!=0 //定义a初始化变量为0
+        var b:Double!=0 //定义b初始化变量为0
+        var c:Double!=0 //定义c初始化变量为0
+        
+        if(!x.text!.isEmpty) {  //text内不能为空
+            a=(x.text! as NSString).doubleValue //类型转换为double
+        }
+        if(!y.text!.isEmpty) {
+            b=(y.text! as NSString).doubleValue
+        }
+        c=a+b
+        z.text="\(c)"
+        
+    }
+    @IBOutlet weak var z: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var x: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
